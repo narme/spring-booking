5e07c1ef311835d054efa4deb65405a4223e6dc2
kang-jisoo <kjsoo0402@naver.com>
2021-05-04 20:52:18 +0900

    Spring Setting

---
+++ Booksys/src/test/java/Team4/Booksys/BooksysApplicationTests.java
@@ -0,0 +1,13 @@
+package Team4.Booksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
