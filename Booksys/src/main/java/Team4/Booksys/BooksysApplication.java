fc2a85bd8b2e7b462d14d88bacf55a9d05008edc
Lee wk <Lee wk@DESKTOP-04U8QH2>
2021-05-14 13:21:10 +0900

    불필요한 코드 제거

--- Booksys/src/main/java/Team4/Booksys/BooksysApplication.java
+++ Booksys/src/main/java/Team4/Booksys/BooksysApplication.java
@@ -3,18 +3,12 @@ package Team4.Booksys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import Team4.mariadb.connectionTest;
-
 @SpringBootApplication
 public class BooksysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksysApplication.class, args);
-		
-		//db연동테스트//
-		connectionTest test = new connectionTest();
-		test.viewtest();
-		/////////////
+
 	}
 
 }
