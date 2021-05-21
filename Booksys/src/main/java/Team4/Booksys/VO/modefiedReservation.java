ed2bf4bf568603facb48fd5f4970fb13d57bb838
Lee wk <Lee wk@DESKTOP-04U8QH2>
2021-05-22 06:14:24 +0900

    예약 조회에 쓰일 객체 modefiedReservation 구현 및 추가

---
+++ Booksys/src/main/java/Team4/Booksys/VO/modefiedReservation.java
@@ -0,0 +1,24 @@
+package Team4.Booksys.VO;
+
+public class modefiedReservation {
+	public int oid;
+	public int people_number;
+	public String start_time;
+	public int rank;
+	public int tid;
+	
+	public void setVal_oid(int oid){
+		this.oid=oid;
+	}
+	public void setVal_people_number(int people_number){
+		this.people_number=people_number;
+	}
+	public void setVal_start_time(String start_time){
+		this.start_time=start_time;
+	}
+	public void setVal_rank(int rank){
+		this.rank=rank;
+	}public void setVal_tid(int tid){
+		this.tid=tid;
+	}
+}
