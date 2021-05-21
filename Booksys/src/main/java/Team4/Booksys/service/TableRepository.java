bbbb856005c09dfe8c4d7d303ab8727a432cf82a
Lee wk <Lee wk@DESKTOP-04U8QH2>
2021-05-22 06:06:07 +0900

    테이블 리포지토리에 테이블의 개수를 구하는 쿼리함수 추가

--- Booksys/src/main/java/Team4/Booksys/service/TableRepository.java
+++ Booksys/src/main/java/Team4/Booksys/service/TableRepository.java
@@ -1,11 +1,14 @@
  package Team4.Booksys.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import Team4.Booksys.VO.TableVO;
 
 
 
 public interface TableRepository extends JpaRepository<TableVO, Long> {
-		
+	@Query("SELECT COUNT(*) FROM TABLE1")
+	public int numberOfTable();
+	
 }
