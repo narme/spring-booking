3c18bb87c4a7b21a1f41578a478830a1655f98da
lahezy <jouhi3322@gmail.com>
2021-05-22 17:08:25 +0900

    amdin으로 로그인 추가(페이지 추가)+ join시 공백체크

--- Booksys/src/main/java/Team4/Booksys/controller/UserController.java
+++ Booksys/src/main/java/Team4/Booksys/controller/UserController.java
@@ -19,9 +19,9 @@ import Team4.Booksys.VO.ReservationVO;
 import Team4.Booksys.VO.modefiedReservation;
 import Team4.Booksys.service.LoginService;
 import Team4.Booksys.service.ReservationService;
+import Team4.Booksys.service.TableService;
 import Team4.Booksys.service.UserRepository;
 import Team4.Booksys.service.UserService;
-import Team4.Booksys.service.TableService;
 
 @Controller
 public class UserController {
@@ -31,18 +31,25 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
+	@ResponseBody // return to body
 	@RequestMapping(value = "/joinUs.do", method = RequestMethod.POST)
 	public String joinUs(HttpServletRequest req, CustomerVO vo) {
 		vo.setVal_id(req.getParameter("id"));
 		if (userRepository.findById(vo.getVal_id()) != null) {
 			System.out.println("중복아이디 감지");
-			return "failed";
+			return "<script> alert('중복된 아이디 입니다.');  location.href= '/join'; </script>";
 		}
 		vo.setVal_password(req.getParameter("PASSWORD"));
 		vo.setVal_name(req.getParameter("name"));
 		vo.setVal_phonenumber(req.getParameter("phonenumber"));
+		
+		/*확인 부분*/
+		if(vo.getVal_id()==""||vo.getVal_name()==""||vo.getVal_password()==""||vo.getVal_phonenumber()=="")
+			return "<script> alert('정보를 모두 입력해주세요.');  location.href= '/join'; </script>";
+		/*끝*/
+		
 		userService.joinUser(vo);
-		return "/index";
+		return "<script> location.href= '/index'; </script>";
 	}
 
 	@RequestMapping(value = "/join")
@@ -55,12 +62,12 @@ public class UserController {
 		System.out.print("nooo..");
 		return "failed";
 	}
-	
+
 	// login
 	@Autowired
 	LoginService loginService;
 
-	@ResponseBody //return to body
+	@ResponseBody // return to body
 	@PostMapping(value = "/signIn.do", produces = "text/html; charset=UTF-8")
 	public String signIn(HttpSession session, HttpServletRequest req) {
 		String id = req.getParameter("id");
@@ -78,16 +85,21 @@ public class UserController {
 
 		if (loginService.loginCheck(id, pw)) {
 			System.out.print("\n" + id + "님 login");
-			
-			//유저의 oid도 세션에 함께 저장한다. (DB연동관련) 
+
+			// 유저의 oid도 세션에 함께 저장한다. (DB연동관련)
 			CustomerVO vo = userRepository.findById(id);
 			int oid = vo.getVal_oid();
 			session.setAttribute("oid", oid);
-			//코드끝 
-			
+			// 코드끝
+
 			session.setAttribute("loginCheck", true);
 			session.setAttribute("id", id);
-			return "<script> alert('로그인 되셨습니다!');  location.href= '/home'; </script>";
+			session.setAttribute("level", vo.getVal_level());
+			
+			if (vo.getVal_level() == 0)
+				return "<script> alert('로그인 되셨습니다!');  location.href= '/home'; </script>";
+			else //관리자 
+				return "<script> alert('로그인 되셨습니다!');  location.href= '/adminhome'; </script>";
 			// return "/home";
 
 		} else {
@@ -98,26 +110,45 @@ public class UserController {
 
 	}
 
-	//logout
+	// logout
 	@RequestMapping(value = "/logOut.do")
 	public String logOut(HttpSession session) {
 		session.setAttribute("loginCheck", null);
 		session.setAttribute("id", null);
+		session.setAttribute("level", null);
 		return "/index";
 	}
 
 	// page mapping
-	@RequestMapping(value = "/home",  produces = "text/html; charset=UTF-8")
-	public String home(HttpSession session) {
-		if(session.getAttribute("loginCheck") == null)return "index";
+	@RequestMapping(value = "/home", produces = "text/html; charset=UTF-8")
+	public String home(HttpSession session,Model model) {
+		if (session.getAttribute("loginCheck") == null)
+			return "index";
+		
+		
+		model.addAttribute("userid", session.getAttribute("id"));
 		return "home";
 	}
-
+	
+	
+	@RequestMapping(value = "/adminhome")
+	public String adminhome(HttpSession session,Model model) {
+		/*관리자 레벨인지 확인*/
+		if (session.getAttribute("loginCheck") == null || (int)session.getAttribute("level")==0)
+			return "index";
+		
+		model.addAttribute("userid", session.getAttribute("id"));
+		return "adminhome";
+	}
+	
+	
 	@RequestMapping(value = "/index")
 	public String index() {
 		return "index";
 	}
+	
 
+	
 	@RequestMapping(value = "/eventReservation")
 	public String eventReservation() {
 		return "eventReservation";
@@ -127,15 +158,15 @@ public class UserController {
 	public String noEventReservation() {
 		return "noEventReservation";
 	}
-	
+
 	@RequestMapping(value = "/showTableView")
 	public String showTableView() {
 		return "showTableView";
 	}
-	
+
 	@RequestMapping(value = "/showUserReservation")
-	public String showUserReservation(HttpServletRequest request, Model model) { //예약리스트 조회관련 코드 추가함 ㅁㅁ
-		HttpSession session = request.getSession(true);//현재 세션 로드
+	public String showUserReservation(HttpServletRequest request, Model model) { // 예약리스트 조회관련 코드 추가함 ㅁㅁ
+		HttpSession session = request.getSession(true);// 현재 세션 로드
 		int currentOid = (int) session.getAttribute("oid");
 		String currentid = (String) session.getAttribute("id");
 		List<ReservationVO> list = ReservationService.getReservationList(currentOid);
@@ -158,50 +189,49 @@ public class UserController {
 		model.addAttribute("userid", currentid);
 		return "showUserReservation";
 	}
-	
+
 	@Autowired
 	ReservationService ReservationService;
-	
+
 	@Autowired
 	TableService TableService;
+
 	@RequestMapping(value = "/addReservation")
 	public String addReservation(HttpServletRequest request, ReservationVO vo) {
-		
-		
-		HttpSession session = request.getSession(true);//현재 세션 로드
-		vo.setVal_uid((int) session.getAttribute("oid"));//세션의 oid값 가져오기
-		vo.setVal_people_number(Integer.parseInt(request.getParameter("num_people")));//인원수 가져오기
-		
-		String date = request.getParameter("date");//날짜 가져오기
-		String time = request.getParameter("time");//시간 가져오기
-		String datetime = date +" "+time;
-		vo.setVal_start_time(datetime);//날짜 + 시간 가져오기
-		
-		//자동배정의 tid 구하기
+
+		HttpSession session = request.getSession(true);// 현재 세션 로드
+		vo.setVal_uid((int) session.getAttribute("oid"));// 세션의 oid값 가져오기
+		vo.setVal_people_number(Integer.parseInt(request.getParameter("num_people")));// 인원수 가져오기
+
+		String date = request.getParameter("date");// 날짜 가져오기
+		String time = request.getParameter("time");// 시간 가져오기
+		String datetime = date + " " + time;
+		vo.setVal_start_time(datetime);// 날짜 + 시간 가져오기
+
+		// 자동배정의 tid 구하기
 		int tid = 1;
 		int mintablewait = 9999;
-		int numOftable = TableService.numberofTable(); //테이블의개수
-		for(int j = 1; j<=numOftable;j++) {
+		int numOftable = TableService.numberofTable(); // 테이블의개수
+		for (int j = 1; j <= numOftable; j++) {
 			int i = ReservationService.findWaitRank(datetime, j);
-			if(i < mintablewait) {
+			if (i < mintablewait) {
 				mintablewait = i;
 				tid = j;
 			}
 		}
-		
-		int i = ReservationService.findWaitRank(datetime, tid);//동일날짜 동시간대에 있는 예약의 개수 리턴
-		if(i!=0)/*이미 해당시간에 예약이 존재한다면*/{
-			vo.setVal_wait(1);//예약이 존재한다.
-			vo.setVal_rank(i);//대기순서는 i
-		}
-		else/*해당시간에 예약이 없다면 */{
+
+		int i = ReservationService.findWaitRank(datetime, tid);// 동일날짜 동시간대에 있는 예약의 개수 리턴
+		if (i != 0)/* 이미 해당시간에 예약이 존재한다면 */ {
+			vo.setVal_wait(1);// 예약이 존재한다.
+			vo.setVal_rank(i);// 대기순서는 i
+		} else/* 해당시간에 예약이 없다면 */ {
 			vo.setVal_wait(0);
 			vo.setVal_rank(0);
 		}
-			
+
 		vo.setVal_tid(tid);
 		ReservationService.addReservation(vo);
-		
+
 		return "home";
 	}
 }
