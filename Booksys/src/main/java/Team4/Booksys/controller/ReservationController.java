40d82d807e2bc76744634236c64bb41b953464e6
Lee wk <Lee wk@DESKTOP-04U8QH2>
2021-05-25 15:06:13 +0900

    예약 취소 부분 구현

--- Booksys/src/main/java/Team4/Booksys/controller/ReservationController.java
+++ Booksys/src/main/java/Team4/Booksys/controller/ReservationController.java
@@ -1,10 +1,12 @@
 package Team4.Booksys.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,4 +26,12 @@ public class ReservationController {
 		ReservationService.removeReservationForUser(vo);
 		return "<script> alert('취소 완료');  location.href= '/showUserReservation'; </script>";
 	}
+	
+	@RequestMapping(value = "/callDeleteReserve/{oid}", produces = "text/html; charset=UTF-8")
+	public String callDeleteReserve(@PathVariable int oid,HttpSession session) {
+		ReservationVO vo = ReservationService.findByOid(oid);
+		ReservationService.removeReservationForUser(vo);
+		return "redirect:/showUserReservation";			
+	}
+	
 }
