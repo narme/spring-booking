a9d9b0be97e0853153c0d476d0ce6f4b179e9032
Lee wk <Lee wk@DESKTOP-04U8QH2>
2021-05-26 16:40:37 +0900

    이벤트 수정을 위한 쿼리문임

--- Booksys/src/main/java/Team4/Booksys/service/EventRepository.java
+++ Booksys/src/main/java/Team4/Booksys/service/EventRepository.java
@@ -1,9 +1,18 @@
 package Team4.Booksys.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import Team4.Booksys.VO.EventVO;
 
 public interface EventRepository extends JpaRepository<EventVO, Long> {
+	 public EventVO findByRid(int rid);
 	 
+	 @Transactional
+	 @Modifying(clearAutomatically = true)
+	 @Query("UPDATE EVENT SET event_type = ?1, event_song = ?2, event_memo = ?3 WHERE rid = ?4" )
+	 public void updateEvent(String event_type, String event_song, String event_memo, int rid);
 }
